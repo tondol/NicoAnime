dda170875039c8e5c255b12349d58943be6322ef
tondol <tondol@gmail.com>
2014-10-23 17:43:08 +0900

    INSTALL.sqlの更新

--- INSTALL.sql
+++ INSTALL.sql
@@ -1,37 +1,49 @@
+--
+-- テーブルの構造 `channels`
+--
+
 CREATE TABLE IF NOT EXISTS `channels` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`nicoChannelId` varchar(255) NOT NULL,
-	`title` varchar(255) NOT NULL,
-	`description` text NOT NULL,
-	`keywords` text NOT NULL,
-	`createdAt` datetime NOT NULL,
-	`modifiedAt` datetime DEFAULT NULL,
-	`crawledAt` datetime DEFAULT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `nicoChannelId` (`nicoChannelId`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nicoChannelId` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `keywords` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `modifiedAt` datetime DEFAULT NULL,
+  `crawledAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nicoChannelId` (`nicoChannelId`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+--
+-- テーブルの構造 `logs`
+--
 
 CREATE TABLE IF NOT EXISTS `logs` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`kind` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`message` text NOT NULL,
-	`createdAt` datetime NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kind` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
+--
+-- テーブルの構造 `videos`
+--
+
 CREATE TABLE IF NOT EXISTS `videos` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `channelId` int(11) NOT NULL,
-	  `nicoVideoId` varchar(255) NOT NULL,
-	  `title` varchar(255) NOT NULL,
-	  `description` text NOT NULL,
-	  `filename` varchar(255) DEFAULT NULL,
-	  `filesize` int(11) DEFAULT NULL,
-	  `retryCount` int(11) NOT NULL,
-	  `createdAt` datetime NOT NULL,
-	  `downloadedAt` datetime DEFAULT NULL,
-	  `deletedAt` datetime DEFAULT NULL,
-	  PRIMARY KEY (`id`),
-	  UNIQUE KEY `nicoVideoId` (`nicoVideoId`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channelId` int(11) NOT NULL,
+  `nicoVideoId` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `retryCount` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `downloadedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nicoVideoId` (`nicoVideoId`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
